a024e7a2e8b596010afe1171a1865347ddefc505
LocCongNghe <117754394+LocCongNghe@users.noreply.github.com>
2024-05-14 22:00:12 +0700

    Thêm sản phẩm vào homepage

--- ecom/store/models.py
+++ ecom/store/models.py
@@ -1,11 +1,17 @@
 from django.db import models
 import datetime
+
 # Categories of Products
 class Category(models.Model):
 	name = models.CharField(max_length=50)
 
 	def __str__(self):
 		return self.name
+
+	#@daverobb2011
+	class Meta:
+		verbose_name_plural = 'categories'
+
 
 # Customers
 class Customer(models.Model):
@@ -45,4 +51,3 @@ class Order(models.Model):
 
 	def __str__(self):
 		return self.product
-
