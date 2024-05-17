be2f8f4c44f28480d2523d9c27e2491d2ca2e2b4
LocCongNghe <117754394+LocCongNghe@users.noreply.github.com>
2024-05-17 10:59:23 +0700

    cart

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -54,6 +54,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'cart.context_processors.cart',
+
             ],
         },
     },
@@ -110,8 +111,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = ['static/']
 
+
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
